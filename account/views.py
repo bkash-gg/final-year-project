06ceb0c718a8378fb3214221b5f7ef8d0ab5be03
bkash-gg <np01cp4s230121@islingtoncollege.edu.np>
2024-09-29 21:46:58 +0545

    URL pattern modified

--- account/views.py
+++ account/views.py
@@ -5,4 +5,4 @@ def login_page(request):
     return render(request, 'account/login.html')
 
 def signup_page(request):
-    return render(request, 'account/singup.html')
+    return render(request, 'account/signup.html')
